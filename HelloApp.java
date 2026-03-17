16f27ee203dabc4484ce9bb9130360b4f7750fb6
Aditi-Singh-7 <aditisingh95061@gmail.com>
2026-03-17 23:09:37 +0530

    uc:3

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,11 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Assign name from argument or default to "World"
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
     }
 }
